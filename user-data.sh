b03678cb6eac74bda621314459ea1ca80a912f4c
Vladimir Sudilovsky <vsudilovsky@gmail.com>
2014-04-24 16:00:44 -0400

    Add example user-data script

---
+++ user-data.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+apt-get update
+apt-get install -y python-pip git
+pip install pip --upgrade
+pip install boto --upgrade
+
+git clone https://github.com/adsabs/adsabs-aws /adsabs-aws
+
+/usr/bin/python  /adsabs-aws/aws_provisioner.py --zookeeper
