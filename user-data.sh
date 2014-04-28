8fd610ca473a79e103049b6791ac5a558979f8a4
Vladimir Sudilovsky <vsudilovsky@gmail.com>
2014-04-28 15:56:28 -0400

    user-data provided in config.py

--- user-data.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-apt-get update
-apt-get install -y python-pip git
-pip install --upgrade pip boto
-
-git clone https://github.com/adsabs/adsabs-aws /adsabs-aws
-/usr/bin/python  /adsabs-aws/aws_provisioner.py --zookeeper
