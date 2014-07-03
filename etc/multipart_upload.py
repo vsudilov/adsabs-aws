40705765581097ab85b2c0e9cd8d277d236b0f50
Vladimir Sudilovsky <vsudilovsky@gmail.com>
2014-07-03 09:57:32 -0400

    Add multipart_upload.py

---
+++ etc/multipart_upload.py
@@ -0,0 +1,33 @@
+#http://boto.readthedocs.org/en/latest/s3_tut.html#storing-large-data
+import math, os
+import boto
+from filechunkio import FileChunkIO
+
+# Connect to S3
+ak = os.environ.get('AWS_ACCESS_KEY',None)
+sk = os.environ.get('AWS_SECRET_KEY',None)
+c = boto.connect_s3(aws_access_key_id=ak,aws_secret_access_key=sk)
+b = c.get_bucket('mybucket')
+
+# Get file info
+source_path = '/media/vsudilov/usbdisk/solr-index/solrIndex.tar.gz'
+source_size = os.stat(source_path).st_size
+
+# Create a multipart upload request
+mp = b.initiate_multipart_upload(os.path.basename(source_path))
+
+# Use a chunk size of 50 MiB (feel free to change this)
+chunk_size = 52428800*2
+chunk_count = int(math.ceil(source_size / chunk_size))
+
+# Send the file parts, using FileChunkIO to create a file-like object
+# that points to a certain byte range within the original file. We
+# set bytes to never exceed the original file size.
+for i in range(chunk_count + 1):
+  offset = chunk_size * i
+  bytes = min(chunk_size, source_size - offset)
+  with FileChunkIO(source_path, 'r', offset=offset,bytes=bytes) as fp:
+    mp.upload_part_from_file(fp, part_num=i + 1)
+
+# Finish the upload
+mp.complete_upload()
