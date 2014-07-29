1de1231c8614c5536efc32b4e136c493a3a5d646
Vladimir Sudilovsky <vsudilovsky@gmail.com>
2014-07-29 09:21:14 -0400

    Remove EBS prewarm via dd

--- components/Solr.py
+++ components/Solr.py
@@ -46,11 +46,11 @@ class Solr:
       cmd = ['mkfs','-t','ext4','/dev/xvdf']
       P = subprocess.Popen(cmd)
       P.wait()
-    print "Warming EBS"
-    start = time.time()
-    P = subprocess.Popen(['dd','if=/dev/xvdf','of=/dev/null','bs=1M'])
-    P.wait()
-    print "Done in %0.1f seconds" % (time.time()-start)
+    # print "Warming EBS"
+    # start = time.time()
+    # P = subprocess.Popen(['dd','if=/dev/xvdf','of=/dev/null','bs=1M'])
+    # P.wait()
+    # print "dd completed in %0.1f seconds" % (time.time()-start)
     utils.mkdir_p('/data')
     cmd = ['mount','/dev/xvdf','/data']
     P = subprocess.Popen(cmd)
