9e68867b0742777d33fdf4ae2d83942debf52878
Vladimir Sudilovsky <vsudilovsky@gmail.com>
2015-03-16 19:27:57 +0000

    refactor: cleanup; clean up UserScript.py

--- aws_provisioner.py
+++ aws_provisioner.py
@@ -3,9 +3,6 @@ import argparse
 import config
 
 from components import Solr
-from components import Zookeeper
-from components import GlobalProvisioner
-from components import EB
 from components import UserScript
 
 
@@ -16,42 +13,6 @@ def main(argv=sys.argv):
     default=False,
     action='store_true',
     dest='zookeeper',
-  )
-  parser.add_argument(
-    '--solr',
-    default=False,
-    action='store_true',
-    dest='solr',
-  )
-  parser.add_argument(
-    '--adslogging',
-    default=False,
-    action='store_true',
-    dest='adslogging',
-  )
-  parser.add_argument(
-    '--adslogging-forwarder',
-    default=False,
-    action='store_true',
-    dest='adslogging_forwarder',
-  )
-  parser.add_argument (
-    '--global',
-    default=False,
-    action='store_true',
-    dest='global_provision',
-  )
-  parser.add_argument(
-    '--application',
-    default=None,
-    nargs=3,
-    dest='EB',
-    metavar=('name','path','version_string'),
-    help='\n'.join([
-      'name: name of application (must have a corresponding entry in config.py)',
-      'path: path to application root directory',
-      'version_string: user generated tag to mark this upload',
-      ]),
   )
 
   parser.add_argument(
@@ -67,34 +28,11 @@ def main(argv=sys.argv):
       ]),
   )
 
-
   args = parser.parse_args()
-
-  if args.global_provision:
-    P = GlobalProvisioner.GlobalProvisioner(config)
-    P.orderedProvision()
 
   if args.zookeeper:
     P = Zookeeper.Zookeeper()
     P.localProvision()
-
-  if args.solr:
-    P = Solr.Solr()
-    P.localProvision()
-
-  if args.adslogging:
-    P = Adslogging.Adslogging()
-    P.localProvision()
-
-  if args.adslogging_forwarder:
-    P = Adslogging.Adslogging()
-    P.localProvision(certpath='/adslogging_forwarder/certs/')
-
-  if args.EB:
-    app,path,version_string = args.EB
-    P = EB.EB(config,app,path,version_string)
-    P.localProvision()
-    P.remoteProvision()
 
   if args.user_script:
     tags,key,script = args.user_script
--- components/UserScript.py
+++ components/UserScript.py
@@ -1,5 +1,6 @@
 import os,sys,time
 import subprocess
+import datetime
 
 import boto
 import boto.ec2
@@ -8,23 +9,38 @@ import utils
 
 class UserScript:
   
-  def __init__(self,tags,key,script):
+  def __init__(self,tags,key,script,user='ubuntu'):
     self.c = utils.connect(boto.ec2.connection.EC2Connection)
     tags = tags.split(':')
     self.tags = {'key':tags[0],'value':tags[1]}
     self.key = os.path.abspath(key)
     self.script = os.path.abspath(script)
+    self.tmpfile = 'userscript_%{time}.sh'.format(
+      time=datetime.datetime.utcnow().strftime('%m.%d.%Y_%H:%M:%S')
+    )
+    self.user = user
 
-  def localProvision(self):
+  def run(self):
     instances = [i for i in self.c.get_only_instances() if i.tags.get(self.tags['key'],None) == self.tags['value'] and i.state=="running"]
     for i in instances:
-      rsync = "rsync -vrPa -e 'ssh -C -i %(key)s' %(script)s ubuntu@%(remote)s:/tmp/UserScript.sh" % {'script':self.script,'remote':i.ip_address, 'key':self.key}
-      ssh = "ssh -i %(key)s ubuntu@%(remote)s 'bash /tmp/UserScript.sh'" % {'key':self.key,'remote':i.ip_address}
+      rsync = "rsync -vrPa -e 'ssh -C -i %{key}' %{script} %{user}@%{remote}:/tmp/%{tmpfile}".format(
+        key=self.key,
+        user=self.user,
+        script=self.script,
+        remote=i.ip_address,
+        tmpfile=self.tmpfile
+      )
+      ssh = "ssh -i %{key} %{user}@%{remote} 'bash /tmp/%{tmpfile}'".format(
+        key=self.key,
+        remote=i.ip_address,
+        tmpfile=self.tmpfile,
+        user=self.user,
+      )
 
-      print "Running:\n\t%s" % rsync
+      print "Running:\n\t%s\non %s" % (rsync,i)
       P = subprocess.Popen(rsync,shell=True)
       P.wait()
-      print "Running:\n\t%s" % ssh
+      print "Running:\n\t%s\non %s" % (ssh,i)
       P = subprocess.Popen(ssh,shell=True)
       P.wait()
 
